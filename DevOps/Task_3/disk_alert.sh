16bbd0e137ba1a3a6a9d6990d8f7786833555a50
kenguru1337 <90187502+kenguru1337@users.noreply.github.com>
2025-05-20 16:13:29 +0300

    Update disk_alert.sh

--- DevOps/Task_3/disk_alert.sh
+++ DevOps/Task_3/disk_alert.sh
@@ -1,31 +1,32 @@
 #!/usr/bin/env bash
 
-# Параметры
 MSMTP_CONF="/Users/alex/Documents/Работа/Aton/DevOps/disk_alert/msmtprc"
-THRESHOLD=85
+THRESHOLD_FREE=85
 PARTITION="/"
 
-read _ TOTALKB AVAILKB _ < <(df -k "$PARTITION" | awk 'NR==2 {print $2, $4}')
+timestamp(){ date '+%Y-%m-%d %H:%M:%S'; }
 
+# читаем total и avail в KB (ровно две переменные!)
+read TOTALKB AVAILKB < <(df -k "$PARTITION" | awk 'NR==2 {print $2, $4}')
+
+# считаем проценты
+USEDKB=$(( TOTALKB - AVAILKB ))
 FREE=$(( AVAILKB * 100 / TOTALKB ))
-USED=$(( 100 - FREE ))
+USED=$((100 - FREE))
 
-# Проверка, превышает ли использование порог
-if (( FREE < THRESHOLD )); then
+# алерт
+if (( FREE < THRESHOLD_FREE )); then
   SUBJECT="Disk Free Alert: ${FREE}% available"
-  BODY="Warning: Free disk space on $(hostname) is now ${FREE}% (<${THRESHOLD}%) at $(date '+%F %T')."
+  BODY="Warning: free disk space on $(hostname) is ${FREE}% (<${THRESHOLD_FREE}%) at $(timestamp)."
 
+  # Отправка письма
   FULL_MESSAGE="From: test_aton_777@mail.ru
 To:   test_aton_777@mail.ru
 Subject: $SUBJECT
 
 $BODY"
 
-  # Отправка письма
   printf '%s\n' "$FULL_MESSAGE" \
-    | msmtp -v \
-            -C "$MSMTP_CONF" \
-            -a mailru \
-            -t test_aton_777@mail.ru
+    | msmtp -v -C "$MSMTP_CONF" -a mailru -t
 fi
 
